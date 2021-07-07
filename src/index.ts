96a0ec5b49776257fbc32c6b41f7540916a9bddc
Simon Sinclair <simon@simonsinclair.com>
2021-07-08 00:19:26 +0100

    Implement initial n32 algorithm

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
-const n32 = (number: number) => {
-  return number;
+const n32 = (number: number): string => {
+  const SYMBOLS = '0123456789abcdefghjkmnpqrstvwxyz';
+  const BASE = 32;
+  let m = '';
+  let n = number;
+
+  while (n > 0) {
+    m = SYMBOLS.charAt(n % BASE) + m;
+    n = Math.trunc(n / BASE);
+  }
+
+  return m;
 };
 
 export default n32;
