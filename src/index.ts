fdeb2e2da2601937b38c00aa1c1b28369d8e2694
Simon Sinclair <simon@simonsinclair.com>
2021-07-08 23:41:53 +0100

    Separate symbols retrieval logic

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
-const n32 = (number: number): string => {
+export const getSymbol = (index: number): string => {
   const SYMBOLS = '0123456789abcdefghjkmnpqrstvwxyz';
+  if (index > SYMBOLS.length - 1) {
+    throw new Error('"index" exceeds the length of symbols available.');
+  }
+  return SYMBOLS.charAt(index);
+};
+
+const n32 = (number: number): string => {
   const BASE = 32;
   let m = '';
   let n = number;
 
   while (n > 0) {
-    m = SYMBOLS.charAt(n % BASE) + m;
+    m = getSymbol(n % BASE) + m;
     n = Math.trunc(n / BASE);
   }
 
