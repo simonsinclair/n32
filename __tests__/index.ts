6cd9bb485bbc0085a56ec6ac4736182729c6783e
Simon Sinclair <simon@simonsinclair.com>
2021-07-08 23:56:16 +0100

    Add full test coverage

--- __tests__/index.ts
+++ __tests__/index.ts
@@ -1,7 +1,27 @@
-import n32 from '../dist';
+import n32, { getSymbol } from '../dist';
 
 describe('n32', () => {
   it('returns a valid string', () => {
-    expect(n32(12345)).toBe('c1s');
+    expect(n32(0)).toBe('0');
+    expect(n32(31)).toBe('z');
+    expect(n32(32)).toBe('10');
+    expect(n32(Number.MAX_SAFE_INTEGER)).toBe('7zzzzzzzzzz');
+  });
+  it('throws an error', () => {
+    expect(() => n32(-1)).toThrowError('n32 expects an absolute number.');
+  });
+});
+
+describe('getSymbol', () => {
+  it('returns a valid symbol', () => {
+    expect(getSymbol(0)).toBe('0');
+    expect(getSymbol(9)).toBe('9');
+    expect(getSymbol(10)).toBe('a');
+    expect(getSymbol(31)).toBe('z');
+  });
+  it('throws an error', () => {
+    expect(() => getSymbol(32)).toThrowError(
+      '"index" exceeds the length of symbols available.',
+    );
   });
 });
